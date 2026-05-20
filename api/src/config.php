ac938d301f1248fee7e94aa7526e61d7aa197556
rafaeldev <rafaeldev22@gmail.com>
2026-05-20 17:34:11 -0300

    feat: add configs headers and bd access

---
+++ api/src/config.php
@@ -0,0 +1,28 @@
+<?php
+
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+    header("Content-Type: application/json");
+
+    function connect(){
+        $host = getenv('DB_HOST') ?: 'db';
+
+        header("Content-Type: application/json");
+
+        $host = getenv('DB_HOST') ?: 'db';
+        $dbname = getenv('DB_NAME') ?: 'facul_db';
+        $user = getenv('DB_USER') ?: 'flutter_user';
+        $password = getenv('DB_PASSWORD') ?: 'flutter_password';
+
+        try {
+            $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
+            
+            echo json_stream(["status" => "sucesso", "mensagem" => "Conectado ao MySQL com sucesso via Docker!"]);
+        } catch (PDOException $e) {
+            echo json_encode(["status" => "erro", "mensagem" => "Falha na conexão: " . $e->getMessage()]);
+        }
+    }
+
